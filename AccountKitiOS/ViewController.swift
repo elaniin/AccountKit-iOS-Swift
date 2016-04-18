28ab2879c7f736beb48662096da14f5f3054383c
Adrián Gómez <g.adrian@elaniin.com>
2016-04-18 16:01:20 -0600

    Initial Commit

---
+++ AccountKitiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AccountKitiOS
+//
+//  Created by Adrian Gomez on 4/18/16.
+//  Copyright © 2016 Elaniin Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
